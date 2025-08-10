b2b545212b9072602bdb7f6d84b5bc5130239994
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2025-08-10 13:48:39 +1000

    EnvironmentContextDelegator.setEnvironmentValue
    - Closes https://github.com/mP1/walkingkooka-environment/issues/89
    - EnvironmentContextDelegator.setEnvironmentValue

--- src/test/java/walkingkooka/environment/EnvironmentContextDelegatorTest.java
+++ src/test/java/walkingkooka/environment/EnvironmentContextDelegatorTest.java
@@ -20,7 +20,6 @@ package walkingkooka.environment;
 import walkingkooka.environment.EnvironmentContextDelegatorTest.TestEnvironmentContextDelegator;
 
 import java.time.LocalDateTime;
-import java.util.Objects;
 
 public final class EnvironmentContextDelegatorTest implements EnvironmentContextTesting2<TestEnvironmentContextDelegator> {
 
@@ -49,15 +48,6 @@ public final class EnvironmentContextDelegatorTest implements EnvironmentContext
                 LocalDateTime::now,
                 EnvironmentContext.ANONYMOUS
             );
-        }
-
-        @Override
-        public <T> EnvironmentContext setEnvironmentValue(final EnvironmentValueName<T> name,
-                                                          final T value) {
-            Objects.requireNonNull(name, "name");
-            Objects.requireNonNull(value, "value");
-
-            throw new UnsupportedOperationException();
         }
 
         @Override
